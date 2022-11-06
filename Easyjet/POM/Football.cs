5fb5051a02ff3c2aa98246607a2caf9b623b50d0
CheragIrani <cfirani@hotmail.com>
2022-11-06 13:47:55 +0000

    Football page added

---
+++ Easyjet/POM/Football.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Easyjet.Helper;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace Easyjet.POM
+{
+    public class Football
+    {
+        By ScoresAndFixturesLink => By.LinkText("/sport/football/scores-fixtures");
+        By TablesLink => By.LinkText("/sport/football/tables");
+        By GossipLink => By.LinkText("/sport/football/gossip");
+
+        IWebDriver _driver;
+
+        public Football(IWebDriver driver)
+        {
+            _driver = driver;
+            Assert.IsTrue(driver.IsElementDisplayed(ScoresAndFixturesLink) && driver.IsElementDisplayed(TablesLink) && driver.IsElementDisplayed(GossipLink));
+        }
+    }
+}
